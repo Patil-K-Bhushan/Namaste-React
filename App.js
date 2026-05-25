d19e4d6bd11d9029963bd9c0408f56933d951cd7
Bhushan Patil <bpatil.00001@gmail.com>
2026-05-25 17:04:02 +0530

    Episode 2.5: Functional Components, JS inside Functional Components

--- App.js
+++ App.js
@@ -2,8 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // Functional Components
+
+let data = 1000
+
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    Namaste React using JSX
+  </h1>
+)
+
 const Heading = () => {
-  return <h1 className="heading">Namaste React Functional Component</h1>
+  return <div id="container">
+    {Title()}
+    <Title/>
+    <Title></Title>
+    <h2>{data}</h2> //JS inside Functional Components
+  </div>
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
