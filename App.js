bed13007225c5ca7ab295905fbeb0e5b6f047807
Bhushan Patil <bpatil.00001@gmail.com>
2026-05-25 16:03:55 +0530

    Episode 2.2: Core React Element

--- App.js
+++ App.js
@@ -1,36 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-/*
-
-<div id = "parent">
-    <div id = "child1">
-        <h1></h1>
-        <h2></h2>
-    </div>
-    <div id = "child2">
-        <h1></h1>
-        <h22></h2>
-    </div>
-</div>
-
-*/
-
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", 
-      { id: "child1" }, 
-    [ React.createElement("h1", {}, "This is Namaste React 🚀"), React.createElement("h2", {}, "By Akshay Saini")]
-  )
-);
-
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hello World from React!",
-// );
+const heading = React.createElement(
+  "h1",
+  {id: "heading"},
+  "Namaste React 🚀"
+)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
+root.render(heading);
