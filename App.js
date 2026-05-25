fbb7ce46b9cbade24b7026cbad97010aba02c047
Bhushan Patil <bpatil.00001@gmail.com>
2026-05-25 15:05:53 +0530

    Episode 1.1

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 /*
 
 <div id = "parent">
@@ -16,15 +19,10 @@
 const parent = React.createElement(
   "div",
   { id: "parent" },
-  [ React.createElement("div", 
+  React.createElement("div", 
       { id: "child1" }, 
-    [ React.createElement("h1", {}, "I'm an h1 tag"), React.createElement("h2", {}, "I'm an h2 tag")]
-    ),
-    React.createElement("div", 
-      { id: "child2" }, 
-    [ React.createElement("h1", {}, "I'm an h1 tag"), React.createElement("h2", {}, "I'm an h2 tag")]
-    )
-  ]
+    [ React.createElement("h1", {}, "This is Namaste React 🚀"), React.createElement("h2", {}, "By Akshay Saini")]
+  )
 );
 
 // const heading = React.createElement(
