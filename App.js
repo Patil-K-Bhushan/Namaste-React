f52a492985197ccc6c6e46a8a6253a7dfd3f3949
Bhushan Patil <bpatil.00001@gmail.com>
2026-05-25 11:52:26 +0530

    Episode 00 0.2

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 /*
 
 <div id = "parent">
-    <div id = "child">
+    <div id = "child1">
         <h1></h1>
         <h2></h2>
+    </div>
+    <div id = "child2">
+        <h1></h1>
+        <h22></h2>
     </div>
 </div>
 
@@ -12,10 +16,15 @@
 const parent = React.createElement(
   "div",
   { id: "parent" },
-  React.createElement("div", 
-    { id: "child" }, 
-    [React.createElement("h1", {}, "I'm an h1 tag"), React.createElement("h2", {}, "I'm an h2 tag")]
-  )
+  [ React.createElement("div", 
+      { id: "child1" }, 
+    [ React.createElement("h1", {}, "I'm an h1 tag"), React.createElement("h2", {}, "I'm an h2 tag")]
+    ),
+    React.createElement("div", 
+      { id: "child2" }, 
+    [ React.createElement("h1", {}, "I'm an h1 tag"), React.createElement("h2", {}, "I'm an h2 tag")]
+    )
+  ]
 );
 
 // const heading = React.createElement(
