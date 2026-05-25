ff1d3d8009bffdece0cbdc6690e7199056b5e4c8
Bhushan Patil <bpatil.00001@gmail.com>
2026-05-25 16:21:28 +0530

    Episode 2.3: JSX, ES6 engine, parcel, Babel

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// Core React Element
 const heading = React.createElement(
   "h1",
   {id: "heading"},
   "Namaste React 🚀"
-)
+);
+console.log (heading);
+
+// JSX Element
+const jsxHeading = <h1 id="heading">Namaste React</h1>;
+console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(jsxHeading);
