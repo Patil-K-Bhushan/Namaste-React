4142f7c46697988862e52c0b89d0821c22da8aa1
Bhushan Patil <bpatil.00001@gmail.com>
2026-05-25 16:42:46 +0530

    Episode 2.4: React Functional Components

--- App.js
+++ App.js
@@ -1,17 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// Core React Element
-const heading = React.createElement(
-  "h1",
-  {id: "heading"},
-  "Namaste React 🚀"
-);
-console.log (heading);
-
-// JSX Element
-const jsxHeading = <h1 id="heading">Namaste React</h1>;
-console.log(jsxHeading);
+// Functional Components
+const Heading = () => {
+  return <h1 className="heading">Namaste React Functional Component</h1>
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<Heading/>);
