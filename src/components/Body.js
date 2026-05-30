ca812953a83a0affa910173c240cc638e175e19b
Bhushan Patil <bpatil.00001@gmail.com>
2026-05-30 14:11:51 +0530

    Error: Loading Restaurants issue solving

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,65 +1,96 @@
-import { useEffect, useState } from 'react';
-import MindSection from './MindSection';
-import RestaurantChain from './RestaurantChain';
-import Restaurants from './Restaurants';
-import { SWIGGY_API } from '../utils/constants';
-import useOnlineStatus from '../utils/useOnlineStatus';
+import { useEffect, useState } from "react";
+import MindSection from "./MindSection";
+import RestaurantChain from "./RestaurantChain";
+import Restaurants from "./Restaurants";
+import { SWIGGY_API } from "../utils/constants";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
-const Body = ()=>{
-    const [mindData, setMindData] = useState();
-    const [chainHeader, setChainHeader] = useState();
-    const [chains, setChains] = useState();
-    const [restaurantsHeader, setRestaurantsHeader] = useState();
-    const [restaurants, setRestaurants] = useState()
+const Body = () => {
+  const [mindData, setMindData] = useState([]);
+  const [chainHeader, setChainHeader] = useState();
+  const [chains, setChains] = useState([]);
+  const [restaurantsHeader, setRestaurantsHeader] = useState();
+  const [restaurants, setRestaurants] = useState([]);
 
-    const fetchData = async () => {
-        const data = await fetch(SWIGGY_API);
-        const json = await data.json();
+  const fetchData = async () => {
+    try {
+      const data = await fetch(SWIGGY_API);
 
-        console.log(json);
+      if (!data.ok) {
+        throw new Error("Failed to fetch");
+      }
 
-        const dishes = json?.data?.cards?.[0]?.card?.card?.gridElements?.infoWithStyle?.info;
-        const chainHeader = json?.data?.cards?.[1]?.card?.card?.header?.title;
-        const chains = json?.data?.cards?.[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
-        const restaurantsHeader = json?.data?.cards?.[2]?.card?.card?.title;
-        const restaurants =  json?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+      const json = await data.json();
 
-        setMindData(dishes);
-        setChainHeader(chainHeader);
-        setChains(chains);
-        setRestaurantsHeader(restaurantsHeader);
-        setRestaurants(restaurants);
+      console.log(json);
+
+      const cards = json?.data?.cards || [];
+
+      const mindCard = cards.find(
+        (c) => c?.card?.card?.id === "whats_on_your_mind",
+      );
+
+      const topRestaurantCard = cards.find(
+        (c) => c?.card?.card?.id === "top_brands_for_you",
+      );
+
+      const restaurantCard = cards.find(
+        (c) => c?.card?.card?.gridElements?.infoWithStyle?.restaurants,
+      );
+
+      setMindData(mindCard?.card?.card?.gridElements?.infoWithStyle?.info);
+
+      setChainHeader(topRestaurantCard?.card?.card?.header?.title);
+
+      setChains(
+        topRestaurantCard?.card?.card?.gridElements?.infoWithStyle?.restaurants,
+      );
+
+      setRestaurantsHeader(restaurantCard?.card?.card?.title);
+
+      setRestaurants(
+        restaurantCard?.card?.card?.gridElements?.infoWithStyle?.restaurants,
+      );
+    } catch (err) {
+      console.error("Fetch Error:", err);
     }
+  };
 
-    useEffect(()=>{
-        fetchData();
-    },[]);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const onlineStatus = useOnlineStatus();
+  const onlineStatus = useOnlineStatus();
 
-    if(onlineStatus === "false"){
-        <h1>Look's like you are offline!! Please, check your Internet Connection</h1>
-    }
+  if (onlineStatus === false) {
+    return (
+      <h1>
+        Looks like you are offline!! Please check your Internet Connection
+      </h1>
+    );
+  }
 
-    return(
-        <div className='Body'>
+  return (
+    <div className="Body">
+      {/* Whats on your Mind Section */}
+      <div className="Mind-Section">
+        <MindSection dishes={mindData} />
+      </div>
 
-            {/* Whats on your Mind Section */}
-            <div className='Mind-Section'>
-                <MindSection dishes={mindData}/>
-            </div>
+      {/* Top Restaurants Chains in Location Section */}
+      <div className="Restaurant-Chains">
+        <RestaurantChain ChainHeader={chainHeader} Chains={chains} />
+      </div>
 
-            {/* Top Restaurants Chains in Location Section */}
-            <div className='Restaurant-Chains'>
-                <RestaurantChain ChainHeader={chainHeader} Chains={chains} />
-            </div>
-
-            {/* Restaurants with Online food delivery in Loaction */}
-            <div className='Restaurants'>
-                <Restaurants RestaurantHeader={restaurantsHeader} Restaurants={restaurants} />
-            </div>
-        </div>
-    )
-}
+      {/* Restaurants with Online food delivery in Loaction */}
+      <div className="Restaurants">
+        <Restaurants
+          RestaurantHeader={restaurantsHeader}
+          Restaurants={restaurants}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Body;
